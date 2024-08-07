3218a645d57b2bd2ff8ca47a170233ef06516bab
swhart22 <s.w.hart22@gmail.com>
2024-08-07 09:50:14 -0400

    updates to soundcloud component

--- src/lib/Soundcloud/index.svelte
+++ src/lib/Soundcloud/index.svelte
@@ -2,24 +2,40 @@
     export let block;
     let URL = block.URL;
 </script>
-
+<div class="pad">
 <div class="soundcloud-container">
-    <iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1890874221&color=%23ff5500&inverse=false&auto_play=false&show_user=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/injusticewatch" title="Injustice Watch" target="_blank" style="color: #333333; text-decoration: none;">Injustice Watch</a> · <a href="https://soundcloud.com/injusticewatch/the-tenant-trap-part-1-rising-rents-unlivable-apartments" title="The Tenant Trap - Part 1: Rising rents, unlivable apartments" target="_blank" style="color: #333333; text-decoration: none;">The Tenant Trap - Part 1: Rising rents, unlivable apartments</a></div>
+    <p class="listen">Listen to this story:</p>
+    <iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1890874221&color=%23ea6d59&inverse=false&auto_play=false&show_user=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/injusticewatch" title="Injustice Watch" target="_blank" style="color: #333333; text-decoration: none;">Injustice Watch</a> · <a href="https://soundcloud.com/injusticewatch/the-tenant-trap-part-1-rising-rents-unlivable-apartments" title="The Tenant Trap - Part 1: Rising rents, unlivable apartments" target="_blank" style="color: #333333; text-decoration: none;">The Tenant Trap - Part 1: Rising rents, unlivable apartments</a></div>
+</div>
 </div>
 
 
 <style lang='scss'>
    
+   .pad {
+    padding-left: 10px;
+    padding-right: 10px;
+    max-width: 780px;
+    margin: 1rem auto;
+   }
     .soundcloud-container {
-        max-width: 780px;
-        margin: 1rem auto;
-        padding-left:10px;
-        padding-right: 10px;
+        
+        padding-left:2.375rem;
+        padding-right: 2.375rem;
+        padding-top: 1.25rem;
+        padding-bottom: 1.25rem;
         box-sizing: border-box;
         background-color: #f5f5f5;
-        padding: 1rem;
+        
         border-radius: 2px;
         border: 1px solid #e5e5e5;
+        .listen {
+            color: #333;
+            font-family: 'Sofia Pro', sans-serif;
+            margin: 0;
+            margin-bottom: 1.6rem;
+            font-size: 0.8rem;
+        }
         div {
             margin-top: 1.6rem;
             color: #635E77 !important;
